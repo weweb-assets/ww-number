36d2c56a763f21a5d16f306d5b6044a27d6f996c
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-06-14 12:54:23 +0200

    v1

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="ww-number"></div>
+  <div class="ww-number">
+    <wwElement v-bind="content.numberElement" :wwProps="{ text: value }" />
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     content: { type: Object, required: true },
+  },
+  data() {
+    return {
+      datas: null,
+    };
+  },
+  computed: {
+    value() {
+      const value = this.content.value;
+      const locale = this.content.locale;
+      const options = {
+        style: this.content.style,
+        currency: this.content.currency,
+        currencyDisplay: this.content.currencyDisplay,
+        notation: this.content.notation,
+        unit: this.content.unit,
+        unitDisplay: this.content.unitDisplay,
+        minimumIntegerDigits: this.content.minimumIntegerDigits,
+        minimumFractionDigits: this.content.fractionDigits,
+        maximumFractionDigits: this.content.fractionDigits,
+        useGrouping: this.content.thousandsSeparator,
+      };
+      return new Intl.NumberFormat(locale, options).format(value);
+    },
   },
 };
 </script>
--- ww-config.js
+++ ww-config.js
@@ -1,8 +1,238 @@
+import locales from "./src/locales";
+import currencies from "./src/currencies";
+
 export default {
   editor: {
     label: {
       en: "Number",
     },
+    icon: "number",
+    bubble: {
+      icon: "number",
+    },
+    customSettingsPropertiesOrder: [
+      "value",
+      "locale",
+      [
+        "style",
+        "currency",
+        "currencyDisplay",
+        "notation",
+        "signDisplay",
+        "unit",
+        "unitDisplay",
+      ],
+      [
+        "minimumIntegerDigits",
+        "fractionDigits",
+        "thousandsSeparator",
+        "convertion",
+      ],
+    ],
   },
-  properties: {},
+  properties: {
+    value: {
+      label: {
+        en: "Value",
+      },
+      type: "Text",
+      bindable: true,
+      defaultValue: 3.14159,
+      section: "settings",
+    },
+    locale: {
+      label: {
+        en: "Locale",
+        fr: "Locale",
+      },
+      type: "TextSelect",
+      options: {
+        options: locales,
+      },
+      defaultValue: "en",
+      section: "settings",
+      bindable: true,
+    },
+    style: {
+      label: {
+        en: "Style",
+        fr: "Style",
+      },
+      type: "TextSelect",
+      options: {
+        options: [
+          { value: "decimal", label: "Decimal" },
+          { value: "currency", label: "Currency" },
+          { value: "percent", label: "Percent" },
+          { value: "unit", label: "Unit" },
+        ],
+      },
+      defaultValue: "currency",
+      section: "settings",
+    },
+    currency: {
+      hidden: (content) => content.style !== "currency",
+      label: {
+        en: "Currency",
+        fr: "Currency",
+      },
+      type: "TextSelect",
+      options: {
+        options: currencies,
+      },
+      defaultValue: "USD",
+      section: "settings",
+      bindable: true,
+    },
+    currencyDisplay: {
+      hidden: (content) => content.style !== "currency",
+      label: {
+        en: "Currency display",
+        fr: "Currency display",
+      },
+      type: "TextSelect",
+      options: {
+        options: [
+          { value: "narrowSymbol", label: "Symbol" },
+          { value: "code", label: "Code" },
+          { value: "name", label: "Name" },
+        ],
+      },
+      defaultValue: "narrowSymbol",
+      section: "settings",
+    },
+    notation: {
+      label: {
+        en: "Notation",
+        fr: "Notation",
+      },
+      type: "TextSelect",
+      options: {
+        options: [
+          { value: "standard", label: "Standard" },
+          { value: "scientific", label: "Scientific" },
+          { value: "compact", label: "Compact" },
+        ],
+      },
+      defaultValue: "standard",
+      section: "settings",
+    },
+    unit: {
+      hidden: (content) => content.style !== "unit",
+      label: {
+        en: "Unit",
+        fr: "Unit",
+      },
+      type: "TextSelect",
+      options: {
+        options: [
+          { value: "acre", label: "acre" },
+          { value: "bit", label: "bit" },
+          { value: "byte", label: "byte" },
+          { value: "celsius", label: "celsius" },
+          { value: "centimeter", label: "centimeter" },
+          { value: "day", label: "day" },
+          { value: "degree", label: "degree" },
+          { value: "fahrenheit", label: "fahrenheit" },
+          { value: "fluid-ounce", label: "fluid-ounce" },
+          { value: "foot", label: "foot" },
+          { value: "gallon", label: "gallon" },
+          { value: "gigabit", label: "gigabit" },
+          { value: "gigabyte", label: "gigabyte" },
+          { value: "gram", label: "gram" },
+          { value: "hectare", label: "hectare" },
+          { value: "hour", label: "hour" },
+          { value: "inch", label: "inch" },
+          { value: "kilobit", label: "kilobit" },
+          { value: "kilobyte", label: "kilobyte" },
+          { value: "kilogram", label: "kilogram" },
+          { value: "kilometer", label: "kilometer" },
+          { value: "liter", label: "liter" },
+          { value: "megabit", label: "megabit" },
+          { value: "megabyte", label: "megabyte" },
+          { value: "meter", label: "meter" },
+          { value: "mile", label: "mile" },
+          { value: "mile-scandinavian", label: "mile-scandinavian" },
+          { value: "milliliter", label: "milliliter" },
+          { value: "millimeter", label: "millimeter" },
+          { value: "millisecond", label: "millisecond" },
+          { value: "minute", label: "minute" },
+          { value: "month", label: "month" },
+          { value: "ounce", label: "ounce" },
+          { value: "percent", label: "percent" },
+          { value: "petabyte", label: "petabyte" },
+          { value: "pound", label: "pound" },
+          { value: "second", label: "second" },
+          { value: "stone", label: "stone" },
+          { value: "terabit", label: "terabit" },
+          { value: "terabyte", label: "terabyte" },
+          { value: "week", label: "week" },
+          { value: "yard", label: "yard" },
+          { value: "year", label: "year" },
+        ],
+      },
+      defaultValue: "celsius",
+      section: "settings",
+      bindable: true,
+    },
+    unitDisplay: {
+      hidden: (content) => content.style !== "unit",
+      label: {
+        en: "Unit dysplay",
+        fr: "Unit dysplay",
+      },
+      type: "TextSelect",
+      options: {
+        options: [
+          { value: "long", label: "Long" },
+          { value: "short", label: "Short" },
+          { value: "narrow", label: "Narrow" },
+        ],
+      },
+      defaultValue: "short",
+      section: "settings",
+    },
+    minimumIntegerDigits: {
+      type: "Number",
+      label: {
+        en: "Minimum integer digits",
+        fr: "Minimum integer digits",
+      },
+      options: {
+        min: 1,
+        max: 21,
+      },
+      defaultValue: 1,
+      section: "settings",
+      bindable: true,
+    },
+    fractionDigits: {
+      type: "Number",
+      label: {
+        en: "Decimal places",
+        fr: "Decimal places",
+      },
+      options: {
+        min: 0,
+        max: 20,
+      },
+      defaultValue: 2,
+      section: "settings",
+      bindable: true,
+    },
+    thousandsSeparator: {
+      label: {
+        en: "1000 separator",
+        fr: "1000 separator",
+      },
+      type: "OnOff",
+      defaultValue: true,
+      bindable: true,
+      section: "settings",
+    },
+    numberElement: {
+      hidden: true,
+      defaultValue: { isWwObject: true, type: "ww-text" },
+    },
+  },
 };
